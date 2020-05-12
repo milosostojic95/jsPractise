cd32e17761a09a8388aa732b5643db618505d93a
Milos Ostojic <ostoja_95_cacak@hotmail.com>
2020-05-12 13:16:01 +0200

    Added function for prime number

--- main.js
+++ main.js
@@ -11,3 +11,26 @@ function isPolinrome(word) {
    
 }
 isPolinrome('niin');
+
+// prime number function 
+function test_prime(n)
+{
+
+  if (n===1) {
+    return false;
+  }
+  else if(n === 2) {
+    return true;
+  } else {
+    for(var x = 2; x < n; x++) {
+      if(n % x === 0) {
+        return false;
+        }
+    }
+    return true;  
+  }
+}
+
+console.log(test_prime(37));
+
+// question function
