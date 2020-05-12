828238337e4d15606529a8aa6b26c7bfea832418
Milos Ostojic <ostoja_95_cacak@hotmail.com>
2020-05-12 14:48:32 +0200

    Added questions quiz

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+/*
 function isPolinrome(word) {
     const wordCheck = word.split('');
     console.log(wordCheck)
@@ -32,5 +33,48 @@ function test_prime(n)
 }
 
 console.log(test_prime(37));
-
+*/
 // question function
+
+function Questins (question,answers,correct) {
+    this.question = question;
+    this.answers = answers;
+    this.correct = correct;
+}
+
+Questins.prototype.displayQuestion = function() {
+    console.log(this.question);
+    for( i=0; i<this.answers.length; i++ ) {
+        console.log(i+':' +this.answers[i]);
+    }
+}
+
+Questins.prototype.checkAnswer = function(ans) {
+    if(ans === this.correct) {
+        console.log('correct answer')
+    } else {
+        console.log('wrong answer')
+    }
+}
+
+const q1 = new Questins ('jel milos jeo?',['yes','no'],'1');
+const q2 =  new Questins ('sta je milos zavrsio?',['saobracajni','georafksi','medicina'],0);
+const q3 = new Questins ('je l najjaci proigamer?',['yes','no'],1);
+
+const questions = [q1,q2,q3];
+
+function nextQuestion() {
+    var n = Math.floor(Math.random() * questions.length);
+    questions[n].displayQuestion();
+
+    var answer = prompt('Please select the correct answer.');
+    console.log(answer)
+
+    if(answer !== 'exit') {
+        questions[n].checkAnswer(parseInt(answer));
+        
+        nextQuestion();
+    }
+}
+
+nextQuestion();
